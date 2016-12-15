5b7c4b55a9ab4a87517efbda3c141a12a7087ca2
lymlhhj123 <524964426@qq.com>
2016-12-15 10:49:51 +0800

    custom signals

---
+++ myspider/myspider/mysignals.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8-*-
+
+#自定义的信号
+#item成功存储到数据库就发送这个信号
+item_saved = object()
+#item存储到数据库失败就发送这个信号
+item_saved_failed = object()
+
+#html成功存储到数据库就发送这个信号
+html_saved = object()
+#html存储到数据库失败就发送这个信号
+html_saved_failed = object()

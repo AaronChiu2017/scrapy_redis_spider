9302a4e7fcfd9a2a88459dd24a28aed60f2b15b3
lymlhhj123 <524964426@qq.com>
2016-12-19 10:25:47 +0800

    remove redis operation

--- myspider/myspider/pipelines.py
+++ myspider/myspider/pipelines.py
@@ -4,52 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-from txredisapi import lazyConnectionPool
 from scrapy import signals
-from twisted.internet import defer
 from .connection import BaseAsyncMySQL
 from .mysignals import (item_saved, item_saved_failed)
-
-class MyCustomRedisSpiderPipeline(object):
-	"""
-	scrapy-redis的pipeline是将item放到redis的队列中
-	只要是出于分布式抓取的考虑，可以把不同机器上的item
-	集中放到一个地方。
-	所以自定义的pipeline最好优先级高于redis的pipeline
-	可以做一些item去重，过滤，清洗的工作等
-
-	但如果我们只是使用一台机器抓取，可以禁用scrapy-redis的
-	pipeline，然后直接将item存放到数据库中
-
-	异步执行redis操作
-	"""
-	def __init__(self, crawler, redis):
-		self.crawler = crawler
-		#https://github.com/fiorix/txredisapi
-		self.redis = redis
-
-	@classmethod
-	def from_crawler(cls, crawler):
-		return cls(crawler, crawler.settings.get('TWISTED_REDIS_CONFIG'))
-		
-	@defer.inlineCallbacks
-	def open_spider(self, spider):
-		#使用的是适用于twisted的redis非阻塞链接
-		#如果单独使用，就需要创建一个reactor
-		#scrapy中已经有reactor,所以不需要我们创建
-		self.rc = yield lazyConnectionPool(**self.redis)
-
-	@defer.inlineCallbacks
-	def process_item(self, item, spider):
-    	#所有的操作都是异步的，即都是使用yield,因此都需要用装饰器包装
-		yield self.rc.push('item', item)
-		yield item
-
-	@defer.inlineCallbacks
-	def close_spider(self, spider):
-		yield self.rc.disconnect()
-
 
 #异步执行数据库操作的管道
 class MyCustomMySQLPipeline(BaseAsyncMySQL):
@@ -57,23 +14,23 @@ class MyCustomMySQLPipeline(BaseAsyncMySQL):
 	#所以在2.7中发送信号就使用send_catch_log()
 
 	def process_item(self, item, spider):
+		self.insert(item).addCallback(self.callback)	
+		return item
+
+	def insert(self, item):
+		cmd = 'INSERT INTO douban VALUES (?,?,?)'
 		try:
-			self.insert(item).addCallback(self.callback)
-		except Exception as e:
+			d = self.db.runQuery(cmd, item)
+		except Exception:
 			 self.crawler.signals.send_catch_log(item_saved_failed,
 				                                 spider=spider)
 		else:
 			 self.crawler.signals.send_catch_log(item_saved,
 											     spider=spider)
-		finally:
-			return item
-
-	def insert(self, item):
-		cmd = 'INSERT INTO douban VALUES (?,?,?)'
-		return self.db.runQuery(cmd, item)
+			 return d
 
 	def callback(self, value):
-		self.logger.info()
+		self.logger.info('successfully')
 
 
 
